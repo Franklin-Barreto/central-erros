906e03b1ac19215db0bac4fbba625b698a76bf92
Franklin Barreto <franklin-barreto@hotmail.com>
2020-06-07 19:35:49 -0300

    base do projeto

---
+++ src/test/java/br/com/centralerros/CentralErrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.centralerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralErrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
